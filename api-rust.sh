add0d2761fb4781449138384ebbf928b7c29e5af
OscarJavierEscobarM <160677636+OscarJavierEscobarM@users.noreply.github.com>
2024-10-21 13:02:12 -0600

    Cambio de puerto

--- api-rust.sh
+++ api-rust.sh
@@ -4,9 +4,9 @@ echo "WORKDIR /tmp/app" >> Dockerfile
 echo "ADD Cargo.toml Cargo.lock ./" >> Dockerfile
 echo "ADD src/ ./src" >> Dockerfile
 echo "RUN cargo build --release" >> Dockerfile 
-echo "EXPOSE 8080" >> Dockerfile
+echo "EXPOSE 5050" >> Dockerfile
 echo "CMD ["/tmp/app/target/release/actividades-api"]" >> Dockerfile
 
 docker build . -t actividades-extraescolares-api
-docker run -it -p 8080:8080 actividades-extraescolares-api
+docker run -it -p 5050:5050 actividades-extraescolares-api
 docker ps -a 
