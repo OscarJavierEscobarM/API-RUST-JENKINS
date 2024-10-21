2ebcd97654ffafad899b684cc53bd9887f9f39e2
OscarJavierEscobarM <160677636+OscarJavierEscobarM@users.noreply.github.com>
2024-10-21 13:19:03 -0600

    Modificacion api-rust.sh

--- api-rust.sh
+++ api-rust.sh
@@ -5,8 +5,8 @@ echo "ADD Cargo.toml Cargo.lock ./" >> Dockerfile
 echo "ADD src/ ./src" >> Dockerfile
 echo "RUN cargo build --release" >> Dockerfile 
 echo "EXPOSE 5050" >> Dockerfile
-echo "CMD ["/tmp/app/target/release/actividades-api"]" >> Dockerfile
+echo 'CMD ["/tmp/app/target/release/actividades-api"]' >> Dockerfile
 
 docker build . -t actividades-extraescolares-api
-docker run -it -p 5050:5050 actividades-extraescolares-api
+docker run --rm -it -p 5050:5050 actividades-extraescolares-api
 docker ps -a 
